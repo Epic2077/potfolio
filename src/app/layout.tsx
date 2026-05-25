610af6441a298033526235758dfaaa65c7a5d409
Ashkan <the.epic.2077@gmail.com>
2026-05-25 12:31:17 +0330

    google verification code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,7 +68,7 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    // google: '',
+    google: '1VZgz9ZZXI3xl_O-GD6Rww9IPjzdETFFXx0SLx--xpA',
   },
   icons: {
     icon: [{ url: '/favicon.ico', type: 'image/x-icon' }],
