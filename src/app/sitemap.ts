02a79f37979c29aeaeff1cb8115c12d9aebc34a4
Ashkan Sadeghi <the.epic.2077@gmail.com>
2026-05-30 16:34:04 +0330

    Change single quotes to double quotes in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://ashkansadeghi.vercel.app/';
+  const baseUrl = "https://ashkansadeghi.vercel.app/";
   return [
     {
       url: baseUrl,
