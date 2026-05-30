fb742a9e39918e60bfe8286d5b6d789a2f48e211
Ashkan Sadeghi <the.epic.2077@gmail.com>
2026-05-30 16:30:52 +0330

    Remove environment variable for base URL in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://ashkansadeghi.vercel.app/';
+  const baseUrl = 'https://ashkansadeghi.vercel.app/';
   return [
     {
       url: baseUrl,
